d2003995a3a55c7e24c46e0f7e42b50d4e9dfddd
taylordorin <taylordorin@gmail.com>
2018-04-01 12:27:45 -0400

    starting to work on the cards

--- react/src/components/ServiceCardTile.js
+++ react/src/components/ServiceCardTile.js
@@ -11,13 +11,19 @@ class GeneralInfoTile extends Component {
         <div className='service'>
           <div className='service__wrapper'>
             <div className='service__card'>
-            pop
+              <h6>Year Round Services</h6>
+              <p>Lorem ipsum dolor sit amet, accumsan urbanitas cu eam, ei cibo iisque vix, ut eum posse populo necessitatibus. Ut vis dicta numquam, pro noluisse disputationi mediocritatem et. Ne prima brute labitur sit, an nisl platonem mea, vel velit  placerat gubergren laboramus has ut.</p>
+              <a className="button primary" href="#">Contact Us</a>
             </div>
             <div className='service__card'>
-            pop
+              <h6>Our Story</h6>
+              <p>Lorem ipsum dolor sit amet, accumsan urbanitas cu eam, ei cibo iisque vix, ut eum posse populo necessitatibus. Ut vis dicta numquam, pro noluisse disputationi mediocritatem et. Ne prima brute labitur sit, an nisl platonem mea, vel velit  placerat gubergren laboramus has ut.</p>
+              <a className="button primary" href="#">The Hall's</a>
             </div>
             <div className='service__card'>
-            pop
+            <h6>Tree Care</h6>
+            <p>Lorem ipsum dolor sit amet, accumsan urbanitas cu eam, ei cibo iisque vix, ut eum posse populo necessitatibus. Ut vis dicta numquam, pro noluisse disputationi mediocritatem et. Ne prima brute labitur sit, an nisl platonem mea, vel velit  placerat gubergren laboramus has ut.</p>
+            <a className="button primary" href="#">More Tips</a>
             </div>
           </div>
         </div>

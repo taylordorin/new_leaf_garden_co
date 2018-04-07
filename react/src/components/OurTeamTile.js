338510687da948a447450edb0333c2d0a8204808
taylordorin <taylordorin@gmail.com>
2018-04-07 16:18:11 -0400

    set up the team tile and added FPO imagery

--- react/src/components/OurTeamTile.js
+++ react/src/components/OurTeamTile.js
@@ -11,36 +11,40 @@ class OurTeamTile extends Component {
         <div className='our-team'>
           <div className="our-team__container">
             <h1>Our Team</h1>
-            <div className="service">
-              <div className="cards cards__product">
 
-                <div className="svc-card">
-                  <div className="svc-card__body">
-                    <h6>Year Round Services</h6>
-                    <p>Lorem ipsum dolor sit amet, accumsan urbanitas cu eam, ei cibo iisque vix, ut eum posse populo necessitatibus. Ut vis dicta numquam, pro noluisse disputationi mediocritatem et. Ne prima brute labitur sit, an nisl platonem mea, vel velit  placerat gubergren laboramus has ut.</p>
+            <div className="team">
+              <div className="teams team__cards">
+
+                <div className="team-card">
+                  <div className="team-card__img">
+                    <img className="portrait" src={assetHelper["abbey_crop.jpg"]}></img>
                   </div>
-                  <div className="button-base">
-                    <a className="btn primary">Contact Us</a>
+                  <div className="team-card__body">
+                    <h3>Abbey Bray</h3>
+                    <h6>Most Awesome Person Title</h6>
+                    <p>Lorem ipsum dolor sit amet, accumsan urbanitas cu eam, ei cibo iisque vix, ut eum posse populo necessitatibus. Ut vis dicta numquam, pro noluisse disputationi mediocritatem et. Ne prima brute labitur sit, an nisl platonem mea, vel velit  placerat gubergren laboramus has ut.</p>
                   </div>
                 </div>
 
-                <div className="svc-card">
-                  <div className="svc-card__body">
-                    <h6>Our Story</h6>
+                <div className="team-card">
+                  <div className="team-card__img">
+                    <img className="portrait" src={assetHelper["toby_crop.jpg"]}></img>
+                  </div>
+                  <div className="team-card__body">
+                    <h3>Tobey Hall</h3>
+                    <h6>Blah Blah Title</h6>
                     <p>Lorem ipsum dolor sit amet, accumsan urbanitas cu eam, ei cibo iisque vix, ut eum posse populo necessitatibus. Ut vis dicta numquam, pro noluisse disputationi mediocritatem et. Ne prima brute labitur sit, an nisl platonem mea, vel velit placerat gubergren laboramus has ut. </p>
                   </div>
-                  <div className="button-base">
-                    <a className="btn primary">The Halls</a>
-                  </div>
                 </div>
 
-                <div className="svc-card">
-                  <div className="svc-card__body">
-                    <h6>Tree Care</h6>
-                    <p>Lorem ipsum dolor sit amet, accumsan urbanitas cu eam, ei cibo iisque vix, ut eum posse populo necessitatibus. Ut vis dicta numquam, pro noluisse disputationi mediocritatem et. Ne prima brute labitur sit, an nisl platonem mea, vel velit  placerat gubergren laboramus has ut.</p>
+                <div className="team-card">
+                  <div className="team-card__img">
+                    <img className="portrait" src={assetHelper["cynthia_crop.jpg"]}></img>
                   </div>
-                  <div className="button-base">
-                    <a className="btn primary">More Tips</a>
+                  <div className="team-card__body">
+                    <h3>Cynthia Hall</h3>
+                    <h6>Blah Title</h6>
+                    <p>Lorem ipsum dolor sit amet, accumsan urbanitas cu eam, ei cibo iisque vix, ut eum posse populo necessitatibus. Ut vis dicta numquam, pro noluisse disputationi mediocritatem et. Ne prima brute labitur sit, an nisl platonem mea, vel velit  placerat gubergren laboramus has ut.</p>
                   </div>
                 </div>
 
